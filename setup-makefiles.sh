47c43756a33cf014be6d816e4fbec7561c462f45
Christopher A. Williamson <you@example.com>
2019-08-19 21:03:05 +0100

    Replaced ROM_NAME_HERE with aosp as sane default

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -15,7 +15,7 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 ROM_ROOT="$MY_DIR"/../../..
 
-HELPER="$ROM_ROOT"/vendor/ROM_NAME_HERE/build/tools/extract_utils.sh
+HELPER="$ROM_ROOT"/vendor/aosp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
